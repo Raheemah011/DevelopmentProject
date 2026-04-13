5066b4d5e02c59ba837ced2b785dd09ceba27799
Raheemah011 <P2835035@my365.dmu.ac.uk>
2026-04-13 17:40:17 +0100

    version 3 - 07/04/2026

--- dashboard.php
+++ dashboard.php
@@ -1,27 +1,194 @@
-<?php
+\<?php
+
+$pageTitle = "Dashboard";
+$ShowNavBar = true;
+include "header.php"; //loads header file
+
 session_start(); //start session to access session variables
 
-//check is theuser is logged in with session variables and see if it exists
 if (!isset($_SESSION["user_email"])) {
     header("Location: index.php"); //if it isnt set go back to the log in page
     exit;
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-<!-- bootstrap for styling -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<body>
+<!--this creates the side bar including logo, menu, logout-->
+  <div class="container-fluid">
+    
+    
+    <div class="row">
+    
+      <div class="col col-2">
+        <div class="row">
+          <div class="col">
+            <img src="/files/logo.png" style="height:150px" alt="Logo" class="img-fluid ms-2 d-block mb-4">            
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col">
+            
+            <div class="row mt-3 ms-2">
+              <div class="col">
+                Menu
+              </div>
+            </div>
 
-</head>
+            <div class="row mt-3 ms-2">
+              <div class="col">
+                Menu
+              </div>
+            </div>
+            
+            <div class="row mt-3 ms-2">
+              <div class="col">
+                Menu
+              </div>
+            </div>
+            
+            <div class="footer row mt-3 ms-2">
+              <div class="col">
+                <a href="../account/logout.php" class="btn btn-danger mt-3">Logout</a>     
+              </div>
+            </div>
 
-<body class="container mt-5">
-<!-- welcome message with users email -->
-<h2>Welcome <?php echo $_SESSION["user_email"]; ?></h2>
 
-<!--log out button -->
-<a href="../account/logout.php" class="btn btn-danger mt-3">Logout</a>
+          </div>        
+        </div>
+      </div>
+        
+     <!--main page is 10 columns big-->
+     <!--put cards on to the page using bootstrap - replace placeholders -->
+     <div class="col col-10">
+     
+        <div class="row mt-3 ms-2">
+          <div class="col">
+            <h2>Welcome <?php echo $_SESSION["user_email"]; ?></h2>
+          </div>
+        </div>
+               
+        <div class="row mt-3 ms-2">
+          
+          <div class="col col-3">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+
+          <div class="col col-3">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+        
+
+          <div class="col col-3">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+        
+
+          <div class="col col-3">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+        
+        </div>
+        
+        
+        <div class="row mt-3 ms-2">
+        
+          <div class="col col-sm-6">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+        
+
+          <div class="col col-sm-6">
+            <div class="card text-center">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <div class="card-footer text-body-secondary">
+                2 days ago
+              </div>
+            </div>                    
+          </div>
+
+
+        </div>
+               
+        
+
+      </div>    
+      
+    
+    </div>
+      
+    
+
+
+  </div>
+
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,15 +9,64 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") { //checks if the suer submitted the 
     $emailaddress = $_POST["emailaddress"]; //retrieves the values the user entered in the form
     $password = $_POST["password"]; //and stores them as variables
 
-    // simple test login
-    if ($emailaddress === "admin" && $password === "password") {		
-		session_start(); //start session to access session variables
-        $_SESSION["user_email"] = $emailaddress; //if the email posted matches the email stored
-        header("Location: dashboard.php"); //direct to  the dashboard
-        exit();
-    } else {
-        $error = "Invalid emailaddress or password";
+    if (empty($error) && empty(trim($emailaddress))) { //trim - removes blank spaces
+        $error = "Email is required"; //if the user hasnt entered info in the email field an error message is returned
     }
+
+    //now check all values are valid - check the email address using regex, pattern matches to see if it matches the correct email format      
+    if (empty($error) && !preg_match('/^[\w\.\-]+@([\w\-]+\.)+[\w\-]{2,4}$/', $emailaddress)) {
+        $error = "Please enter a valid email address."; //if it doesnt match the correct email format it shows this error message
+    }
+
+    if (empty($error) && empty(trim($password))) {
+        $error = "Password is required"; //if the password hasnt been entered it shows this message
+    }
+
+    //if no error is recorded then find the user in the database
+    if (empty($error)) {
+	  
+      //check can we find a user that matches the details in the database and if so, get the record  
+      //prepared statements protect against SQL injection attacks
+      try {
+        $stmt = $pdo->prepare(" 
+            SELECT user_email, user_password FROM user where user_email = ?
+        ");
+
+        //execute the query
+        $stmt->execute([
+            $emailaddress
+        ]);
+          
+        $user = $stmt->fetch(PDO::FETCH_ASSOC); //this gets the user record from the database
+          
+          
+        if (!$user) {			
+          // if no user record is found
+          $error = "Check your login details and try again<br>If you have forgotten your password you can reset it below";
+        }	
+	      
+      }
+      catch (PDOException $e) {
+        $error = $e->getMessage(); //shows the error message
+        // For debugging, this will write an error to the PHP log file 
+        error_log($error);
+      }      
+	  
+      if (empty($error) && !empty($user)) { //if the info is correctt and a record is foud in the DB
+        
+        if (password_verify($password, $user["user_password"])) { //compares plain text password the user entered with hashed password
+          session_start(); //remembers the user for the next page
+          $_SESSION["user_email"] = $user["user_email"]; //saves email in the session
+          
+          header("Location: dashboard.php"); //prevents the user from accesssing the dashboard without logging in
+          exit();
+        }
+        else {
+          $error = "Check your login details and try again<br>If you have forgotten your password you can reset it below";
+        }
+      }  	  
+    }
+  
 }
 ?>
 
@@ -55,10 +104,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") { //checks if the suer submitted the 
 
             <form method="POST">
               <div class="mb-3">
-                <input type="text" name="emailaddress" id="emailaddress" class="form-control" placeholder="Email Address" required>
+                <input type="text" name="emailaddress" id="emailaddress" class="form-control" placeholder="Email Address" value="Raheemah@hotmail.com" required>
               </div>
               <div class="mb-3">
-                <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password" value="password123" required>
               </div>
               <div class="d-grid">
                 <button class="btn btn-primary">Login</button>
